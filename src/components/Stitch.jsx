704bed4a82e3f8569fa3e64518a3333ca87f62b7
Leslie Mejia <lesliemejia110316@gmail.com>
2026-02-27 10:07:22 -0800

    Added card functionality and finalized styles
    -added better button stylings on nav
    - fixed styles of cards
    -added side cat
    -added videos to each card
    -made buttons on nav work

--- src/components/Stitch.jsx
+++ src/components/Stitch.jsx
@@ -1,72 +1,80 @@
 import { useState } from "react";
 import "../index.css";
 import "../css/stich.css";
-//import cat from "../images/hook_cat.png";
-
+import cat from "../images/hook_cat.png";
 const stitches = [
     {
         id: "How-to-Read-Pattern",
         label: "How to Read A Pattern",
         abbreviation: "N/A",
         example: "N/A",
-        translation: "N/A"
+        translation: "N/A",
+        video: "https://www.youtube.com/embed/PZ8uV2-Z-Qg?si=OZdNYDzC8IrY-039"
 
     },
     {
-        id: "Slip-knot",
-        label: "Slip Knot",
-        abbreviation: "Sl knot",
+        id: "Chain",
+        label: "Chain",
+        abbreviation: "CH",
         example: "R1: Sl Knot, CH9 [9]",
-        translation: "Make a slip knot, chain 9 for a total of 9 stitches"
+        translation: "Make a slip knot, chain 9 for a total of 9 stitches",
+        video: "https://www.youtube.com/embed/qQF4OChjP9s?si=3U2EfruyeR5NxEb9" 
     },
     {
         id: "Single-Crochet",
         label: "Single Crochet",
         abbreviation: "SC",
         example: "R3: (sc, inc) x 6 [18]",
-        translation: "(Single-crochet 1, increase) 6 times for a total of 18 stitches"
+        translation: "(Single-crochet 1, increase) 6 times for a total of 18 stitches",
+        video: "https://www.youtube.com/embed/7FcRdxg0aeY?si=CpZ28tFCg98Tplsq" 
     },
     {
         id: "HD-crochet",
         label: "Half-Double Crochet",
         abbreviation: "HDC",
         example: "R1: (hdc, inc) x 6 [18]",
-        translation: "(Half-double-crochet 1, increase) 6 times for a total of 18 stitches"
+        translation: "(Half-double-crochet 1, increase) 6 times for a total of 18 stitches",
+        video: "https://www.youtube.com/embed/f9C1C21MNiM?si=MByEjvqbmJg59gL_" 
     },
     {
         id: "Double-crochet",
         label: "Double Crochet",
         abbreviation: "DC",
         example: "R1: (dc, inc) x 6 [18]",
-        translation: "(Double-crochet 1, increase) 6 times for a total of 18 stitches"
+        translation: "(Double-crochet 1, increase) 6 times for a total of 18 stitches",
+        video: "https://www.youtube.com/embed/a1whu6Gub1M?si=4pY3JIVewwaec8IZ"
     },
     {
         id: "Treble-crochet",
         label: "Treble Crochet",
         abbreviation: "TR",
         example: "R1: (tr, inc) x 6 [18]",
-        translation: "(Treble-crochet 1, increase) 6 times for a total of 18 stitches"
+        translation: "(Treble-crochet 1, increase) 6 times for a total of 18 stitches",
+        video: "https://www.youtube.com/embed/3KtzjAAZHVY?si=GlbPwAqqXhT33Qt-"  
     },
     {
         id: "Increase",
         label: "Increase",
         abbreviation: "INC",
         example: "R1: (sc, inc) x 6 [18]",
-        translation: "(Single-crochet, increase) 6 times for a total of 18 stitches"
+        translation: "(Single-crochet, increase) 6 times for a total of 18 stitches",
+        video: "https://www.youtube.com/embed/CvD1qrrRX5c?si=VtNwYwyNx5V4HhJp"  
     },
     {
         id: "Decrease",
         label: "Decrease",
         abbreviation: "DEC",
         example: "R18: (2 sc, dec) x 6 [18]",
-        translation: "(2 single-crochet, decrease) 6 times for a total of 18 stitches"
+        translation: "(2 single-crochet, decrease) 6 times for a total of 18 stitches",
+        video: "https://www.youtube.com/embed/ZXsiVk52_vA?si=TsmFcesTlbot1z7a" 
     },
     {
         id: "MR",
         label: "Magic Ring",
         abbreviation: "MR",
         example: "R1: 6 sc in a mr [6]",
-        translation: "6 single-crochet in a magic ring for a total of 6 stitches"
+        translation: "6 single-crochet in a magic ring for a total of 6 stitches",
+        video: "https://www.youtube.com/embed/fNyfAtJ3edE?si=1oxYU4rkk3ud46br" 
     }
 ]
 
@@ -82,34 +90,48 @@ function SidenavButton({stitch, isActive, onClick}) {
 //what card looks like
 function StitchCard ({stitch}) { 
     return (
-        <section id = {stitch.id} className = "stitchCard">
-        <h1>{stitch.label}</h1>
-        <div className = "example">
-            <h2>Abbreviation:</h2><p>{stitch.abbreviation}</p>
-        </div>
-        <div className = "example">
-            <h2>Example:</h2><p>{stitch.example}</p><p>{stitch.translation}</p>
-        </div>
-        <div className = "example">
-            <h2>Translation:</h2> <p>{stitch.translation}</p>
-        </div>
-        </section>
+        <section id={stitch.id} className="stitchCard">
+            <div className="card-header">
+                <h1>{stitch.label}</h1>
+            </div>
+            <span className="abbr">{stitch.abbreviation}</span>
+            <div className="cardrow">
+                <span className="cardlabel">Example</span>
+                <p className="cardvalue code">{stitch.example}</p>
+            </div>
+
+            <div className="cardrow">
+                <span className="cardlabel">Translation</span>
+                <p className="cardvalue code">{stitch.translation}</p>
+            </div>
+            {stitch.video &&(
+            <div className = "cardrow">
+                <span className ="cardlabel">Video</span>
+                <iframe className = "cardvideo" src={stitch.video} allowFullScreen />
+            </div>
+        )}
+    </section>
     );
 }
 
-function NavClick(stitch){
-    setActiveID(stitch.id);
-}
 
 export default function StitchesPage(){
     const [activeID, setActiveID] = useState(stitches[0].id);
+
+    //makes scroll work
+    function NavClick(stitch) {
+    setActiveID(stitch.id);
+    const section = document.getElementById(stitch.id);
+    if (section) section.scrollIntoView({ behavior: "smooth" });
+    }
+
     return(
         <div className = "page">
             <aside className="sidenav">
                 <div className="sidenav-sticky">
                     <h2 className="title">Learn Stitches</h2>
                         {stitches.map((stitch) => (
-                    <SidenavButton key={stitch.id} stitch={stitch} />
+                    <SidenavButton key={stitch.id} stitch={stitch}  isActive={activeID === stitch.id} onClick={() => NavClick(stitch)} />
                     ))}
                 </div>
             </aside>
@@ -118,7 +140,9 @@ export default function StitchesPage(){
                         <StitchCard key = {stitch.id} stitch = {stitch} /> //same thing as before but always makes the squares for the info
                 ))}
             </main>
+            <aside className="cat">
+            <img src={cat} alt="cat" />
+            </aside>
         </div>
-
     );
 }
