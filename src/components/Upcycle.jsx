80a35fa8edb55e0af2d2e39c48535f15028527ad
plandry240z <159398849+plandry240z@users.noreply.github.com>
2026-03-03 14:22:28 -0800

    updated links

--- src/components/Upcycle.jsx
+++ src/components/Upcycle.jsx
@@ -25,14 +25,14 @@ function Upcycle() {
         {
             title: "Reusing Yarn",
             description: "If there are old projects you have, you can unravel them and reuse the yarn for new projects! This is a great way to reduce waste and save money. Just make sure to check the condition of the yarn before reusing it, as some yarns may become damaged or weakened after being used.",
-            video: "https://youtu.be/abc456",
-            resource: "https://www.youtube.com/shorts/a7SptLTO4Rk",
+            video: "https://www.youtube.com/shorts/a7SptLTO4Rk",
+            resource: "https://blog.tincanknits.com/2021/02/04/how-to-recycle-yarn-from-second-hand-sweaters/",
             image: nom_cat
         },
         {
             title: "Why Upcycle vs. Recycle?",
             description: "Upcycling is the process of transforming waste materials or unwanted products into new, higher quality items. It helps reduce waste, conserve resources, and promote sustainability by giving new life to old items. Upcycling can take many forms, from creative DIY projects to innovative design solutions.",
-            video: "https://youtu.be/xyz123",
+            video: "https://youtu.be/9jI27zb35_A",
             resource: "https://www.recyclingtoday.org/blogs/news/upcycling-vs-recycling-key-differences",
             image: recycle_cat
         }
